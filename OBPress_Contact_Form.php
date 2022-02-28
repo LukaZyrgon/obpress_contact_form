df77797ea8ce839d2d4a297c55acba2f8b840153
LukaZyrgon <58442639+LukaZyrgon@users.noreply.github.com>
2022-02-28 10:02:56 +0100

    Update OBPress_Contact_Form.php

--- OBPress_Contact_Form.php
+++ OBPress_Contact_Form.php
@@ -16,12 +16,12 @@ require_once('elementor/init.php');
 
 // TODO, MAKE GIT BRANCH, CONNECT WITH UPDATE CHECKER
 
-// require_once(WP_PLUGIN_DIR . '/OBPress_Demo_Widgets/plugin-update-checker-4.11/plugin-update-checker.php');
-// $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-//     'https://github.com/LukaZyrgon/obpress_demo_widgets',
-//     __FILE__,
-//     'OBPress_Demo_Widgets'
-// );
+require_once(WP_PLUGIN_DIR . '/OBPress_Contact_Form/plugin-update-checker-4.11/plugin-update-checker.php');
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/LukaZyrgon/obpress_contact_form',
+    __FILE__,
+    'OBPress_Demo_Widgets'
+);
 
-// //Set the branch that contains the stable release.
-// $myUpdateChecker->setBranch('main');
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
